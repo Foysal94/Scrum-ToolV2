69ff2a1044da8979308917f00a8fc14d1e3115f8
Foysal94 <Foysal94@outlook.com>
2016-01-05 17:19:59 +0000

    Started on the board, very basic files so far.

---
+++ Controllers/BoardController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ASPNET5_Scrum_Tool.Controllers
+{
+    public class BoardController : Controller
+    {
+        Logger<BoardController> logger;
+        
+        public IActionResult Index(string p_BoardName)
+        {
+            ViewData["Title"] = p_BoardName;
+            return View();
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
