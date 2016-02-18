9871bb619f2c43aff47c1e65057372ce35aea634
Foysal94 <Foysal94@outlook.com>
2016-02-18 17:24:54 +0000

    Used bootstrap to split pages into columns and using bootstrap panels as a possible list/column holder thing

---
+++ src/ASPNET5-Scrum-Tool/Models/BoardModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace ASPNET5_Scrum_Tool.Models
+{
+    public class BoardModel
+    {
+        private string m_BoardName;
+
+        [Required]
+        public string BoardName { get { return m_BoardName; } set { m_BoardName = value; } }
+
+    }
+}
--- src/ASPNET5-Scrum-Tool/wwwroot/_references.js
+++ src/ASPNET5-Scrum-Tool/wwwroot/_references.js
@@ -1,11 +1,8 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="js/Board (1).min.js" />
-/// <reference path="js/board.min (1).js" />
+/// <reference path="../gulpfile.js" />
 /// <reference path="js/Board.min.js" />
-/// <reference path="js/home.min (1).js" />
-/// <reference path="js/Home.min.js" />
-/// <reference path="js/site (1).min.js" />
-/// <reference path="js/site.min (1).js" />
+/// <reference path="js/home.js" />
+/// <reference path="js/site.min (1).min.js" />
 /// <reference path="js/site.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
