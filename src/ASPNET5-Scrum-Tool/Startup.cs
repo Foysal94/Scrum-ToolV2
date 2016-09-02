eac58bb357c51bd02dba7dec5f9d525b4a33105c
Foysal Ahmed <Foysal Ahmed>
2016-09-02 01:31:27 +0100

    Missed the new way DBContext is set up inside ConfigureServices for the previous commit

--- src/ASPNET5-Scrum-Tool/Startup.cs
+++ src/ASPNET5-Scrum-Tool/Startup.cs
@@ -41,12 +41,8 @@ namespace ASPNET5_Scrum_Tool
                     User ID=Foysal94@foysal94;Password=Flatron94;
                     Encrypt=True;TrustServerCertificate=False;
                     Connection Timeout=30;MultipleActiveResultSets=false;";
-            services.AddEntityFramework()
-                .AddSqlServer()
-                .AddDbContext<ScrumToolDB>(options =>
-                {
-                    options.UseSqlServer(azureConnection);
-                });
+            services.AddDbContext<ScrumToolDB>(options =>
+                 options.UseSqlServer(azureConnection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
