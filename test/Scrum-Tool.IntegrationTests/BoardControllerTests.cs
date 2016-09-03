c55dd735cd24fc340b838d8c1019079927c50f45
Foysal Ahmed <foysal94@outlook.com>
2016-09-03 14:46:27 +0100

    Moved the test projects into a seprate folder inside 'test'. Felt more consistent with example structures

---
+++ test/Scrum-Tool.IntegrationTests/BoardControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using ASPNET5_Scrum_Tool;
+using ASPNET5_Scrum_Tool.Controllers;
+using ASPNET5_Scrum_Tool.Models;
+using Microsoft.AspNet.TestHost;
+using Microsoft.Data.Entity;
+using Xunit;
+
+namespace Scrum_Tool.IntegrationTests
+{
+    public class BoardControllerTests :Setup
+    {
+        private BoardController m_BoardController;
+        private ScrumToolDB m_Context;
+
+        public BoardControllerTests()
+        {
+            // Arrange
+            var db = new DbContextOptionsBuilder();
+            db.UseInMemoryDatabase();
+            m_Context = new ScrumToolDB(db.Options);
+            CreateTestData(m_Context);
+            m_BoardController= new BoardController(m_Context);
+
+            m_Server = new TestServer(TestServer.CreateBuilder()
+                .UseStartup<Startup>());
+             m_Client = m_Server.CreateClient();
+
+        }
+
+        [Fact]
+        public void ReturnLoadBoard()
+        {
+            var response = m_Client.GetAsync("/Board/Load/1");
+
+        }
+        
+    }
+}
---
+++ test/Scrum-Tool.IntegrationTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Scrum_Tool.IntegrationTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Scrum_Tool.IntegrationTests")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b527924c-f0b3-4b3f-9c18-f189f18574ea")]
