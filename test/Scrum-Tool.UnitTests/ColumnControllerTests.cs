4f03c1526f4fb293f9ac1b8ebafa7c5d3f18b328
Foysal Ahmed <foysal94@outlook.com>
2016-09-05 15:20:48 +0100

    Successfully created and passed a test to delete a column

--- test/Scrum-Tool.UnitTests/ColumnControllerTests.cs
+++ test/Scrum-Tool.UnitTests/ColumnControllerTests.cs
@@ -13,82 +13,91 @@ using GenFu;
 namespace Scrum_Tool.UnitTests
 {
 
-    public class ColumnControllerTests 
-    {
+	 public class ColumnControllerTests 
+	 {
 		  private const int m_BoardID = 0;
 		  private ScrumToolDB m_ScrumToolDB;
 		  private ColumnController m_ColumnController;
-
 		  private IQueryable<Columns> GenerateTestData() 
-          {
-            /*
-            int id = 0;
-            int numberOfColumns = 3;
-            string Name = "TestColumn";
-            
-            A.Configure<Columns>()
-                .Fill(c => c.ID,() => { return id++; })
-                .Fill(c => c.Name, () => { return $"{Name}{id}"; })
-                .Fill(c => c.BoardID,() => { return m_BoardID; });
-            var columns = A.ListOf<Columns>(numberOfColumns);
-            return columns.AsQueryable();
-            */
+		  {
+				/*
+				int id = 0;
+				int numberOfColumns = 3;
+				string Name = "TestColumn";
+				
+				A.Configure<Columns>()
+					 .Fill(c => c.ID,() => { return id++; })
+					 .Fill(c => c.Name, () => { return $"{Name}{id}"; })
+					 .Fill(c => c.BoardID,() => { return m_BoardID; });
+				var columns = A.ListOf<Columns>(numberOfColumns);
+				return columns.AsQueryable();
+				*/
 
-              List<Columns> columns = new List<Columns>()
-              {
-                  new Columns("TestColumn0", m_BoardID),
-                  new Columns("TestColumn1", m_BoardID),
-                  new Columns("TestColumn2", m_BoardID), 
-              };
+				List<Columns> columns = new List<Columns>()
+				{
+					new Columns("TestColumn0", m_BoardID),
+					new Columns("TestColumn1", m_BoardID),
+					new Columns("TestColumn2", m_BoardID), 
+				};
 
-              return columns.AsQueryable();
-          }
-
-        private DbContextOptions<ScrumToolDB> CreateFakeDatabaseOptions() 
+				return columns.AsQueryable();
+		  }
+		  private DbContextOptions<ScrumToolDB> CreateFakeDatabaseOptions() 
 		  {
 			  // Create a fresh service provider, and therefore a fresh 
-            // InMemory database instance.
-            var serviceProvider = new ServiceCollection()
-                .AddEntityFrameworkInMemoryDatabase()
-                .BuildServiceProvider();
+				// InMemory database instance.
+				var serviceProvider = new ServiceCollection()
+					 .AddEntityFrameworkInMemoryDatabase()
+					 .BuildServiceProvider();
 
-            // Create a new options instance telling the context to use an
-            // InMemory database and the new service provider.
-            var builder = new DbContextOptionsBuilder<ScrumToolDB>();
-            builder.UseInMemoryDatabase()
-                   .UseInternalServiceProvider(serviceProvider);
+				// Create a new options instance telling the context to use an
+				// InMemory database and the new service provider.
+				var builder = new DbContextOptionsBuilder<ScrumToolDB>();
+				builder.UseInMemoryDatabase()
+						 .UseInternalServiceProvider(serviceProvider);
 
-            return builder.Options;
-		  }
+				return builder.Options;
+		 }
+		 public ColumnControllerTests() 
+		 {
+				var dbOptions = CreateFakeDatabaseOptions();
+				var testColumnList = GenerateTestData();
 
-        public ColumnControllerTests() 
-        {
-			  var dbOptions = CreateFakeDatabaseOptions();
-			  var testColumnList = GenerateTestData();
+				m_ScrumToolDB = new ScrumToolDB(dbOptions);
+				m_ScrumToolDB.Columns.AddRange(testColumnList);
+				m_ScrumToolDB.SaveChanges();
+				m_ColumnController = new ColumnController(m_ScrumToolDB);
+		}
 
-			  m_ScrumToolDB = new ScrumToolDB(dbOptions);
-			  m_ScrumToolDB.Columns.AddRange(testColumnList);
-			  m_ColumnController = new ColumnController(m_ScrumToolDB);
-        }
+		[Fact]
+		public void DeleteColumn()
+		{
+				// Arrange
+				Columns testColumn = m_ScrumToolDB.Columns.Last();
+				// Act
+			   m_ColumnController.Delete(testColumn.ID);
+			   // Assert
+			   m_ScrumToolDB.Columns.Should().HaveCount(2, "Inital column list count is 3, and deleted one")
+					 .And.NotContain(testColumn);
+					 
+		}
 
-        [Fact]
-        public void AddColumn()
-        {
-            //Act
-            Columns testColumn = new Columns("Something", m_BoardID);
-            //Arrange
-            m_ColumnController.AddColumn(testColumn);
-            //Assert
-            m_ScrumToolDB.Columns.Should().NotBeNullOrEmpty()
-                .And.HaveCount(4, "Number of inital columns created plus one");
+		[Fact]
+		public void AddColumn()
+		{
+			  	// Arrange
+			   Columns testColumn = new Columns("Something", m_BoardID);
+				//Act
+			   m_ColumnController.AddColumn(testColumn);
+			   // Assert
+			   m_ScrumToolDB.Columns.Should().NotBeNullOrEmpty()
+					 .And.HaveCount(4, "Number of inital columns created plus one");
+			   m_ScrumToolDB.Columns.Last().ShouldBeEquivalentTo(testColumn, options =>
+						  options.Excluding(c => c.ID));
+		}
+		  
 
-            m_ScrumToolDB.Columns.Last().ShouldBeEquivalentTo(testColumn, options =>
-                    options.Excluding(c => c.ID));
+	 }
 
-        }
-
-
-
-    }
 }
 
